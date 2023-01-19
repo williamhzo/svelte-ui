d7a20c706a3ca086ffb828be6e35833cbbf1e16c
William H <william.hermozo@protonmail.com>
2023-01-19 17:34:54 +0000

    create dummy button

---
+++ src/components/button/Button.story.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import Button from './Button.svelte';
+
+	export let Hst: Hst;
+</script>
+
+<Hst.Story>
+	<Button>button</Button>
+</Hst.Story>
